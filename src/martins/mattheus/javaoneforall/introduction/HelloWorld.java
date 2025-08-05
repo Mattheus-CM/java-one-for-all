28305a9d7cf08202c44b16e46887b4502a59da67
Mattheus Martins <mattheus_cmartins@hotmail.com>
2025-08-05 07:58:32 -0300

    Organizing my code into packages and using comments in my code

--- src/martins/mattheus/javaoneforall/introduction/HelloWorld.java
+++ src/martins/mattheus/javaoneforall/introduction/HelloWorld.java
@@ -1,3 +1,8 @@
+package martins.mattheus.javaoneforall.introduction;
+
+/**
+ * This is the HelloWorld class and this is a <b>Javadoc</b>
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! This is my first code in IntelliJ");
