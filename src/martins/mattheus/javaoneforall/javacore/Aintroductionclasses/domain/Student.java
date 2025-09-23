5d84d2f024ade97279e899b0ee161b5ceadd88ea
Mattheus Martins <mattheus_cmartins@hotmail.com>
2025-09-22 21:14:10 -0300

    Introduction Classes

---
+++ src/martins/mattheus/javaoneforall/javacore/Aintroductionclasses/domain/Student.java
@@ -0,0 +1,7 @@
+package martins.mattheus.javaoneforall.javacore.Aintroductionclasses.domain;
+
+public class Student {
+    public String name = "Naruto";
+    public int age;
+    public char gender;
+}
