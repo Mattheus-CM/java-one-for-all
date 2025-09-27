9ed116f493360b3e55582e949f21d48d351143df
Mattheus Martins <mattheus_cmartins@hotmail.com>
2025-09-27 11:25:40 -0300

    Reading data from the keyboard and exercise

---
+++ src/martins/mattheus/javaoneforall/javacore/Gassociation/test/InputKeyboardDataTest01.java
@@ -0,0 +1,13 @@
+package martins.mattheus.javaoneforall.javacore.Gassociation.test;
+
+import java.util.Scanner;
+
+public class InputKeyboardDataTest01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String message = input.nextLine();
+        System.out.println(message);
+
+    }
+}
