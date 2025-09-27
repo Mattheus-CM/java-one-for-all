33c5d4cc06bfdecc14021cd5883ebc942b9ec9d7
Mattheus Martins <mattheus_cmartins@hotmail.com>
2025-09-27 12:16:31 -0300

    Association Exercise

--- src/martins/mattheus/javaoneforall/javacore/Gassociation/domain/Professor.java
+++ src/martins/mattheus/javaoneforall/javacore/Gassociation/domain/Professor.java
@@ -2,9 +2,60 @@ package martins.mattheus.javaoneforall.javacore.Gassociation.domain;
 
 public class Professor {
     private String name;
+    private String researchField;
+    private Seminar[] seminars;
 
     public Professor(String name) {
         this.name = name;
+    }
+
+    public Professor(String name, String researchField) {
+        this(name);
+        this.researchField = researchField;
+    }
+
+    public Professor(String name, String researchField, Seminar[] seminars) {
+        this(name, researchField);
+        this.seminars = seminars;
+    }
+
+    public void print() {
+        System.out.println();
+        System.out.println("Professor -> Nome: " + this.name + " - Especialidade: " + this.researchField);
+        if (seminars == null) {
+            System.out.println("Não possui seminários");
+            return;
+        }
+
+        for (Seminar seminar : seminars) {
+            System.out.println();
+            System.out.println("Título do Seminário: " + seminar.getTitle());
+            System.out.println("Local: " + seminar.getPlace().getAddress());
+            if (seminar.getStudents() == null || seminar.getStudents().length == 0) {
+                System.out.println("Não possui Alunos");
+                continue;
+            }
+            System.out.println("\nAlunos");
+            for (Student student: seminar.getStudents()) {
+                System.out.println("Nome: " + student.getName() + " - Idade: " + student.getAge());
+            }
+        }
+    }
+
+    public String getResearchField() {
+        return researchField;
+    }
+
+    public void setResearchField(String researchField) {
+        this.researchField = researchField;
+    }
+
+    public Seminar[] getSeminars() {
+        return seminars;
+    }
+
+    public void setSeminars(Seminar[] seminars) {
+        this.seminars = seminars;
     }
 
     public String getName() {
---
+++ src/martins/mattheus/javaoneforall/javacore/Gassociation/domain/Seminar.java
@@ -0,0 +1,41 @@
+package martins.mattheus.javaoneforall.javacore.Gassociation.domain;
+
+public class Seminar {
+    private String title;
+    private Student[] students;
+    private Place place;
+
+    public Seminar(String title, Place place) {
+        this.title = title;
+        this.place = place;
+    }
+
+    public Seminar(String title, Place place, Student[] students) {
+        this(title, place);
+        this.students = students;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+}
