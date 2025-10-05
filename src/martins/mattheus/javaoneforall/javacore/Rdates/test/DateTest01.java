7ab880ec631c72b56295c6fd65e31113cd290a2c
Mattheus Martins <mattheus_cmartins@hotmail.com>
2025-10-05 13:21:33 -0300

    Date, Calendar, DateFormat, Date with Locale, Numbers with Locale, Currency with Locale

---
+++ src/martins/mattheus/javaoneforall/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package martins.mattheus.javaoneforall.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1759620580585L);
+        System.out.println(date);
+    }
+}
