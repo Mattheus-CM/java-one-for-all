41501ad220407da58ec7ccbd225de1ccafe6bb9e
Mattheus Martins <mattheus_cmartins@hotmail.com>
2026-01-18 15:53:24 -0300

    Predicate and Behavior Parameterization

---
+++ src/martins/mattheus/javaoneforall/javacore/ZZBparameterization/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package martins.mattheus.javaoneforall.javacore.ZZBparameterization.interfaces;
+
+import martins.mattheus.javaoneforall.javacore.ZZBparameterization.domain.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
---
+++ src/martins/mattheus/javaoneforall/javacore/ZZBparameterization/test/BehaviorParameterizationTest03.java
@@ -0,0 +1,37 @@
+package martins.mattheus.javaoneforall.javacore.ZZBparameterization.test;
+
+import martins.mattheus.javaoneforall.javacore.ZZBparameterization.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class BehaviorParameterizationTest03 {
+
+    private static List<Car> cars = List.of(
+            new Car("green", 2011),
+            new Car("black", 1998),
+            new Car("red", 2019));
+
+    public static void main(String[] args) {
+        List<Car> greenCars = filter(cars, car -> car.getColor().equals("green"));
+        List<Car> redCars = filter(cars, car -> car.getColor().equals("red"));
+        List<Car> yearBefore = filter(cars, car -> car.getYear() < 2015);
+        List<Integer> nums = List.of(1,2,3,4,5,6,7,8,9,10);
+        System.out.println();
+        System.out.println(greenCars);
+        System.out.println(redCars);
+        System.out.println(yearBefore);
+        System.out.println(filter(nums, num -> num % 2 == 0));
+    }
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        List<T> filteredList = new ArrayList<>();
+        for (T e : list) {
+            if (predicate.test(e)) {
+                filteredList.add(e);
+            }
+        }
+        return filteredList;
+    }
+}
